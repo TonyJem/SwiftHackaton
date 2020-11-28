d0521012d84a7dc79426f597789ef4bf65b90d93
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-11-28 23:49:23 +0200

    Create Class "Bank" conforming to protocol "BankApi"

--- Bank_ATM_User.playground/Contents.swift
+++ Bank_ATM_User.playground/Contents.swift
@@ -39,6 +39,66 @@ class User: UserData {
     }
 }
 
+class Bank: BankApi {
+    func showUserBalance() {
+        
+    }
+    
+    func showUserToppedUpMobilePhoneCash(cash: Float) {
+        
+    }
+    
+    func showUserToppedUpMobilePhoneDeposite(deposit: Float) {
+        
+    }
+    
+    func showWithdrawalDeposit(cash: Float) {
+        
+    }
+    
+    func showTopUpAccount(cash: Float) {
+        
+    }
+    
+    func showError(error: TextErrors) {
+        
+    }
+    
+    func checkUserPhone(phone: String) -> Bool {
+        
+    }
+    
+    func checkMaxUserCash(cash: Float) -> Bool {
+        
+    }
+    
+    func checkMaxAccountDeposit(withdraw: Float) -> Bool {
+        
+    }
+    
+    func checkCurrentUser(userCardId: String, userCardPin: Int) -> Bool {
+        
+    }
+    
+    func topUpPhoneBalanceCash(pay: Float) {
+        
+    }
+    
+    func topUpPhoneBalanceDeposit(pay: Float) {
+        
+    }
+    
+    func getCashFromDeposit(cash: Float) {
+        
+    }
+    
+    func putCashDeposit(topUp: Float) {
+        
+    }
+    
+    
+}
+
 let egor_pupkin: User = User(userName: "Egor Pupkin",
                                  userCardId: "3339 0039 3312 2222",
                                  userCardPin: 1234,
