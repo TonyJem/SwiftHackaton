8cd27addc4045f36c18d7bbba4b5354c928187d2
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-11-29 15:39:06 +0200

    Make code more structured

--- Bank_ATM_User.playground/Contents.swift
+++ Bank_ATM_User.playground/Contents.swift
@@ -25,6 +25,49 @@ import UIKit
 //      г. Банк выводит сообщение:
 //          Вы выбрали пополнить баланс телефона на Х сумму через наличные/с депозита. У вас списалось с депозита/с наличных и увеличилось на балансе телефона.
 
+//ToDo: Необходимо разобраться с неймингом: Переименовать все сложные или двусмыслнные названия. (Осуществимо если начать переиминовывать названия переменных и методов данных в задании из начала)
+
+// MARK: - Protocols: -
+// MARK: Описание «пользователя» :
+protocol UserData {
+  var userName: String { get }           //Имя пользователя
+  var userCardId: String { get }         //Номер карты
+  var userCardPin: Int { get }           //Пин-код
+  var userCash: Float { get set}         //Наличные пользователя
+  var userBankDeposit: Float { get set}  //Банковский депозит
+  var userPhone: String { get }          //Номер телефона
+  var userPhoneBalance: Float { get set} //баланс телефона
+}
+
+// MARK: Описание «сервера» :
+// Протокол по работе с банком предоставляет доступ к данным пользователя зарегистрированного в банке
+protocol BankApi {
+    
+//    Функции выводящие сообщения (своей внутренней логикой не обладают!)
+  func showUserBalance() // выводит сообщение о балансе
+  func showUserToppedUpMobilePhoneCash(cash: Float) // сколько кеша положили на телефон, банк обрабатывает данные и выводит соответствующее сообщение
+  func showUserToppedUpMobilePhoneDeposite(deposit: Float) // сколько положили на телефон через депозит банка, банк обрабатывает данные и выводит соответствующее сообщение
+  func showWithdrawalDeposit(cash: Float) // Показать сколько пользователь снял с депозита
+  func showTopUpAccount(cash: Float) // Показать на сколько пользователь пополнил депозит
+  func showError(error: TextErrors) // Метод который отвечает за ошибки. Обернуть Все ошики в один метод.
+ 
+//    Функции со своей внутренней логикой:
+  func checkUserPhone(phone: String) -> Bool // Банк проверяет правильный прислали номер телефона, если неправилный то фолс
+  func checkMaxUserCash(cash: Float) -> Bool // Проверяем доступные средства в кармане. Можем ли заплатить столько сколько указываем в переменной кэш
+  func checkMaxAccountDeposit(withdraw: Float) -> Bool // Проверяем доступные средства на депозите. Можем ли снять указанную сумму
+  func checkCurrentUser(userCardId: String, userCardPin: Int) -> Bool // Метод отвечает за проверку самого пользователя
+
+//    Методы отвечающие за получение данных после всех удачно пройденых проверок, и они отвечаю за изменения первичных данных  начальных настройках конкретного Юзера, то есть фиксируются результаты манипулирования с его данными.
+//    ToDo: Вспомнить что такое мутатинк методы и разобраться для чего они нужны здесь
+// Вроде как мутатинг нужен если юзаем структуры, если все на классах, тогда мутатинг как бы и не причем здесь
+    
+//    TODO: Перенести протоколы на верх, навести в них порядок чтоб уборать лишние коментарии а те что надо расположить красиво и читабельно
+  mutating func topUpPhoneBalanceCash(pay: Float) // Производится вычисление нового значения для баланса телефона и соответсвующего уменьшения колличества кеша
+  mutating func topUpPhoneBalanceDeposit(pay: Float) // Производится вычисление нового значения для баланса телефона и соответсвующего уменьшения колличества средств на депозите
+  mutating func getCashFromDeposit(cash: Float) // Производится уменьшение средств на депозите и увеличение кеша
+  mutating func putCashDeposit(topUp: Float) // Производится уменьшение кеша и увеличение средств на депозите
+}
+
 // MARK: - Enums: -
 // Действия, которые пользователь может выбирать в банкомате (имитация кнопок)
 enum UserActions {
@@ -222,7 +265,6 @@ class ATM {
          someBank: BankApi,
          action: UserActions,
          paymentMethod: PaymentMethod? = nil
-//         requiredAmount: Float
          )
     {
         self.userCardId = userCardId
@@ -230,7 +272,6 @@ class ATM {
         self.someBank = someBank
         self.action = action
         self.paymentMethod = paymentMethod
-//        self.requiredAmount = requiredAmount
         self.sendUserDataToBank(userCardId: userCardId,
                                 userCardPin: userCardPin,
                                 actions: action,
@@ -271,7 +312,6 @@ class ATM {
         case let.userPressedTopUpPhoneBalanceBtn(phoneNumber: phoneNumber,
                                                  payment: payment,
                                                  amount: amount):
-            //      б. Проверяется корректно ли введен номер телефона:
             guard someBank.checkUserPhone(phone: phoneNumber) else {
                 someBank.showError(error: .wrongPhoneNr)
                 return
@@ -297,71 +337,6 @@ class ATM {
         }
     }
 }
-
-// MARK: - Protocols: -
-protocol UserData {
-  var userName: String { get }           //Имя пользователя
-  var userCardId: String { get }         //Номер карты
-  var userCardPin: Int { get }           //Пин-код
-  var userCash: Float { get set}         //Наличные пользователя
-  var userBankDeposit: Float { get set}  //Банковский депозит
-  var userPhone: String { get }          //Номер телефона
-  var userPhoneBalance: Float { get set} //баланс телефона
-}
-
-// MARK: Описание «сервера» :
-// Протокол по работе с банком предоставляет доступ к данным пользователя зарегистрированного в банке
-protocol BankApi {
-    
-//    Функции выводящие сообщения (своей внутренней логикой не обладают!)
-  func showUserBalance() // выводит сообщение о балансе
-  func showUserToppedUpMobilePhoneCash(cash: Float) // сколько кеша положили на телефон, банк обрабатывает данные и выводит соответствующее сообщение
-  func showUserToppedUpMobilePhoneDeposite(deposit: Float) // сколько положили на телефон через депозит банка, банк обрабатывает данные и выводит соответствующее сообщение
-  func showWithdrawalDeposit(cash: Float) // Показать сколько пользователь снял с депозита
-  func showTopUpAccount(cash: Float) // Показать на сколько пользователь пополнил депозит
-  func showError(error: TextErrors) // Метод который отвечает за ошибки. Обернуть Все ошики в один метод.
- 
-//    Функции со своей внутренней логикой:
-  func checkUserPhone(phone: String) -> Bool // Банк проверяет правильный прислали номер телефона, если неправилный то фолс
-  func checkMaxUserCash(cash: Float) -> Bool // Проверяем доступные средства в кармане. Можем ли заплатить столько сколько указываем в переменной кэш
-  func checkMaxAccountDeposit(withdraw: Float) -> Bool // Проверяем доступные средства на депозите. Можем ли снять указанную сумму
-  func checkCurrentUser(userCardId: String, userCardPin: Int) -> Bool // Метод отвечает за проверку самого пользователя
-
-//    Методы отвечающие за получение данных после всех удачно пройденых проверок, и они отвечаю за изменения первичных данных  начальных настройках конкретного Юзера, то есть фиксируются результаты манипулирования с его данными.
-//    ToDo: Вспомнить что такое мутатинк методы и разобраться для чего они нужны здесь
-// Вроде как мутатинг нужен если юзаем структуры, если все на классах, тогда мутатинг как бы и не причем здесь
-    
-//    TODO: Перенести протоколы на верх, навести в них порядок чтоб уборать лишние коментарии а те что надо расположить красиво и читабельно
-  mutating func topUpPhoneBalanceCash(pay: Float) // Производится вычисление нового значения для баланса телефона и соответсвующего уменьшения колличества кеша
-  mutating func topUpPhoneBalanceDeposit(pay: Float) // Производится вычисление нового значения для баланса телефона и соответсвующего уменьшения колличества средств на депозите
-  mutating func getCashFromDeposit(cash: Float) // Производится уменьшение средств на депозите и увеличение кеша
-  mutating func putCashDeposit(topUp: Float) // Производится уменьшение кеша и увеличение средств на депозите
-}
-
-
-//ToDo:  Убрать все это когда ненужно будет
-// MARK: - Пример проверки работы : -
-// For more description, watch video at: 28:30
-// Пользовательские данные, которые хранятся на сервере банка (данные были внесены, когда пользователь зарегестрировался в банке)
-//let egor_pupkin: UserData = User(userName: "Egor Pupkin",
-//                                 userCardId: "3339 0039 3312 2222",
-//                                 userCardPin: 1234,
-//                                 userCash: 2234.34,
-//                                 userBankDeposit: 4994.4,
-//                                 userPhone: "+7(889)-393-43-44",
-//                                 userPhoneBalance: -34.44)
-
-//Какой-то банк, в котором зарегистрирован пользователь (в этом банке хранятся данные пользователя)
-//let bankClient = BankServer(user: egor_pupkin)
-
-//Текущий банкомат, с которым мы работаем в данный момент / пользователь вводит данные и пытается выполнить некоторую операцию.
-//let atm443 = ATM(userCardId: "3339 0039 3312 2222",
-//                 userCardPin: 1234,
-//                 someBank: bankClient,
-//                 action: .userPressedBalanceBtn) // Сюда так же передавать тип оплаты (при помощи чего пополнить при помощи налички или с депозита, а так же передавать сумму на какю хотим проихвести операцию)
-
-//ToDo: Проявить Свое видение, подумать какие могут возникнуть ошибки при выполнении, расписать как это все реализовать
-//ToDo: Разобраться с неймингом: Переименовать все сложные или двусмыслнные названия
 
 
 // MARK: - Instances: -
